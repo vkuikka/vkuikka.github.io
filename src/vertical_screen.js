3e70e69fa18640f97c58cfb289e64d89982df6d7
vkuikka <villekuikka1@gmail.com>
2022-01-28 22:36:26 +0200

    Changed id to class. Added different width for no column pages with different screen sizes. removed useless variables

--- src/vertical_screen.js
+++ src/vertical_screen.js
@@ -4,20 +4,21 @@ export function	handleVertical() {
 		img[i].style.height = "100%";
 		img[i].style.width = "auto";
 	}
-	var set = $('.image_column');
-	set.each(function( index ) {
+	$('.image_column').each(function() {
 		$(this).css("grid-column", "none");
 		$(this).css("grid-row", "1 / 2");
 	});
-	var set = $('.text_column');
-	set.each(function( index ) {
+	$('.text-column').each(function() {
 		$(this).css("grid-column", "none");
 		$(this).css("grid-row", "2 / 2");
 	});
-	var set = $('.pages');
-	set.each(function( index ) {
+	$('.pages').each(function() {
 		$(this).css("grid-template-columns", "none");
 		$(this).css("grid-template-rows", "50% 50%");
 		$(this).css("font-size", "1.4vh");
+	});
+	$('.no_col').each(function() {
+		$(this).css("width", "50%");
+		$(this).css("left", "25%");
 	});
 }
